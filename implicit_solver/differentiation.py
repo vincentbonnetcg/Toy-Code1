05126973a968a9d3b7b015d27054a487af1262f6
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2018-09-05 21:08:27 +1200

    higher order accuracy for jacobian

--- implicit_solver/differentiation.py
+++ implicit_solver/differentiation.py
@@ -3,54 +3,82 @@
 @description :  Numerical differentiation
 """
 
+# Reference # 
+# Fornberg, Bengt. "Generation of finite difference formulas on arbitrarily spaced grids."
+# Mathematics of computation 51, no. 184 (1988): 699-706.
+
+
 import numpy as np
 
+# Central Difference tables and accuracy order
 STENCIL_SIZE = 1e-6
+FIRST_DERIVATIVE_OFFSET = [[-STENCIL_SIZE, STENCIL_SIZE], 
+                           [-2.0 * STENCIL_SIZE, -STENCIL_SIZE, STENCIL_SIZE, 2.0 * STENCIL_SIZE],
+                           [-3.0 * STENCIL_SIZE, -2.0 * STENCIL_SIZE, -STENCIL_SIZE, STENCIL_SIZE, 2.0 * STENCIL_SIZE, 3.0 * STENCIL_SIZE]]
 
-# Private function to compute the gradient of a function with n arguments
-# Used by numericalJacobian function
-def _numericalGradient(function, argumentId, stencils, *args):
-    argsListR = list(args)
-    argsListL = list(args)   
-    argsListR[argumentId] = np.add(args[argumentId], stencils)
-    argsListL[argumentId] = np.subtract(args[argumentId], stencils)
-    valueR = function(*argsListR)
-    valueL = function(*argsListL)
-    gradient = (valueR - valueL) / (STENCIL_SIZE * 2.0)
-    return gradient
+FIRST_DERIVATIVE_COEFS = [[-0.5, 0.5],
+                          [1/12, -2/3, 2/3, -1/12],
+                          [-1/60, 3/20, -3/4, 3/4, -3/20, 1/60]]
+
+ACCURACY_ORDER = 2
+
+# function : function to derivate
+# Accuracy Order can be 2, 4, 6
+def numericalGradient(function, argumentId, componentId, *args):
+    accuracyOrderIndex = (int)(ACCURACY_ORDER / 2) - 1
+    offsets = FIRST_DERIVATIVE_OFFSET[accuracyOrderIndex]
+    coefs = FIRST_DERIVATIVE_COEFS[accuracyOrderIndex]
+
+    argsList = list(args)
+    gradient = None
     
+    if (np.isscalar(args[argumentId])):
+        pass
+        component = argsList[argumentId]
+        stencils = np.add(offsets, component)
+        for i in range(len(stencils)):
+            argsList[argumentId] = stencils[i]
+            if (gradient is None):
+                gradient = np.multiply(function(*argsList), coefs[i])
+            else:
+                gradient += np.multiply(function(*argsList), coefs[i])
+    else:
+        component = argsList[argumentId][componentId]
+        stencils = np.add(offsets, component)
+        for i in range(len(stencils)):
+            argsList[argumentId][componentId] = stencils[i]
+            if (gradient is None):
+                gradient = np.multiply(function(*argsList), coefs[i])
+            else:
+                gradient += np.multiply(function(*argsList), coefs[i])
+
+    gradient /= STENCIL_SIZE
+    return gradient
+
 # This function returns a jacobian matrix with the following dimension :
 # [function codomain dimension, function domain dimension]
 # 'Function codomain dimension' : dimension of the function output
 # 'Function domain dimension' : dimension of the input argumentId of the function
 # Warning : Only use scalar as argument (no integer/boolean)
 def numericalJacobian(function, argumentId, *args):
-    functionCodomainDimension = 1
-    functionDomainDimension = 1
+    functionCodomainDimension = 1 # default when np.isscalar(args[argumentId])
+    functionDomainDimension = 1 # default when np.isscalar(gradientList[0])
 
     gradientList = []
 
-    # compute gradients    
-    if (np.isscalar(args[argumentId])):
-        functionDomainDimension = 1        
-        gradient = _numericalGradient(function, argumentId, STENCIL_SIZE, *args)        
-        gradientList.append(gradient)
-    else:
+    # compute gradients for each component of the argument
+    if (not np.isscalar(args[argumentId])):
         functionDomainDimension = len(args[argumentId])
-        stencils = np.zeros(functionDomainDimension)
-        for i in range(functionDomainDimension):
-            stencils.fill(0)
-            stencils[i] = STENCIL_SIZE
-            gradient = _numericalGradient(function, argumentId, stencils, *args)           
-            gradientList.append(gradient)
+
+    for componentId in range(functionDomainDimension):
+        gradient = numericalGradient(function, argumentId, componentId, *args)
+        gradientList.append(gradient)
 
     # assemble jacobian from gradients
     if len(gradientList)>0:
-        if (np.isscalar(gradientList[0])):
-            functionCodomainDimension = 1
-        else:
+        if (not np.isscalar(gradientList[0])):
             functionCodomainDimension = len(gradientList[0])
-
+           
         jacobian = np.zeros(shape=(functionCodomainDimension, functionDomainDimension))
         for gradientId in range(len(gradientList)):
             jacobian[0:functionCodomainDimension, gradientId] = gradientList[gradientId]
