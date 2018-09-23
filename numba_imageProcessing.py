ed733d057e853dbe34df70b30b845ed4991af8db
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2018-09-23 15:26:29 +1200

    resize image to power2 to increase thread per block

--- numba_imageProcessing.py
+++ numba_imageProcessing.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 from numba import cuda, vectorize
-from skimage import data, io, color
+from skimage import data, io, color, transform
 import skimage as skimage
 import math
 
@@ -29,14 +29,29 @@ def applyKernel(image, imageResult, Gx):
 
     imageResult[x, y] = value
 
+# Resize an image while keeping its ratio
+def resizeImageAndKeepRatio(image, width, height):
+    out  = np.zeros((width, height), dtype=np.float32)
+    scaleX = image.shape[0] / width
+    scaleY = image.shape[1] / height
+    maxScale = max(scaleX, scaleY)
+    newWidth = np.int(image.shape[0] / maxScale)
+    newHeight = np.int(image.shape[1] / maxScale)
+    tmpImage = transform.resize(image, (newWidth, newHeight))
+    offsetX = np.int((width - tmpImage.shape[0]) / 2)
+    offsetY = np.int((height - tmpImage.shape[1]) / 2)
+    out[offsetX:offsetX+tmpImage.shape[0], offsetY:offsetY+tmpImage.shape[1]] = tmpImage
+    return out
+
 # Create a simple image processing
 images = [None, None]
 image = skimage.img_as_float(color.rgb2gray(data.chelsea())).astype(np.float32)
+image = resizeImageAndKeepRatio(image, 512, 512)
 images[0] = image.copy()
 images[1] = image.copy()
 
 # Setup blocks
-threadsPerBlock = (1, 1)
+threadsPerBlock = (16, 16)
 blocksPerGridX = math.ceil(images[0].shape[0] / threadsPerBlock[0])
 blocksPerGridY = math.ceil(images[0].shape[1] / threadsPerBlock[1])
 blocksPerGrid = (blocksPerGridX, blocksPerGridY)
@@ -54,5 +69,4 @@ applyKernel[blocksPerGrid, threadsPerBlock](images[0], images[1], sobelYKernel)
 applyKernel[blocksPerGrid, threadsPerBlock](images[1], images[0], gaussianBlurKernel)
 
 # Show Result
-io.imshow(CombineImages(image, images[0]))
-
+io.imshow(CombineImages(image, images[1]))
