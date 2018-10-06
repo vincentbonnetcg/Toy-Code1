16430989b41966a4f999d67502ab341c393565cb
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2018-10-07 08:53:22 +1300

    small cleanup on the main

--- implicit_solver/main.py
+++ implicit_solver/main.py
@@ -15,30 +15,31 @@ NUM_SUBSTEP = 10 # number of substep per frame
 RENDER_FOLDER_PATH = "" # specify a folder to export png files
 # Used command  "magick -loop 0 -delay 4 *.png out.gif"  to convert from png to animated gif
 
-'''
- Run
-'''
 def main():
+    '''
+    Creates a scene and a solver + solve
+    '''
     # Create scene and solver
-    scene = system.createBeamScene()
+    scene = system.createWireScene()
     solver = system.ImplicitSolver(FRAME_TIMESTEP / NUM_SUBSTEP, NUM_SUBSTEP)
-    #solver = sl.SemiImplicitSolver(FRAME_TIMESTEP / NUM_SUBSTEP, NUM_SUBSTEP) #- only debugging - unstable with high stiffness
-    
+    # below only debugging - unstable with high stiffness
+    #solver = sl.SemiImplicitSolver(FRAME_TIMESTEP / NUM_SUBSTEP, NUM_SUBSTEP)
+
     # Run simulation and render
     render = tools.Render()
     render.setRenderFolderPath(RENDER_FOLDER_PATH)
-    
+
     profiler = tools.profiler.ProfilerSingleton()
-    for frameId in range(0, NUM_FRAME+1):
+    for frame_id in range(0, NUM_FRAME+1):
         profiler.clearLogs()
-    
-        if frameId > 0:
+
+        if frame_id > 0:
             solver.solveFrame(scene)
-    
-        render.showCurrentFrame(scene, frameId)
-        render.exportCurrentFrame(str(frameId).zfill(4) + " .png")
-    
+
+        render.showCurrentFrame(scene, frame_id)
+        render.exportCurrentFrame(str(frame_id).zfill(4) + " .png")
+
         profiler.printLogs()
-  
+
 if __name__ == '__main__':
     main()
