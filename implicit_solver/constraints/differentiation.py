32d5283556521ba4ddeef74b7ccb46a3d3a89c32
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2018-10-13 12:20:52 +1300

    cleanup differentiation

--- implicit_solver/constraints/differentiation.py
+++ implicit_solver/constraints/differentiation.py
@@ -19,69 +19,68 @@ FIRST_DERIVATIVE_COEFS = [[-0.5, 0.5],
                           [1/12, -2/3, 2/3, -1/12],
                           [-1/60, 3/20, -3/4, 3/4, -3/20, 1/60]]
 
-ACCURACY_ORDER = 2
-
-
-def numericalDifferentiation(function, argumentId, componentId, *args):
-    '''
-    function to derivate !!!
-    Accuracy Order can be 2, 4, 6
-    '''
-    accuracyOrderIndex = (int)(ACCURACY_ORDER / 2) - 1
-    offsets = FIRST_DERIVATIVE_OFFSET[accuracyOrderIndex]
-    coefs = FIRST_DERIVATIVE_COEFS[accuracyOrderIndex]
-
-    #argsList = copy.deepcopy(args) # expensive operation => replaced with lines below
-    argsList = []
-    for a in args:
-        argsList.append(np.copy(a))
-
-    array = argsList
-    valueId = argumentId
-    if not np.isscalar(args[argumentId]):
-        array = argsList[argumentId]
-        valueId = componentId
-
-    stencils = offsets + array[valueId]
-
-    # compute gradients
-    array[valueId] = stencils[0]
-    gradient = function(*argsList) * coefs[0]
-
-    for i in range(1, len(stencils)):
-        array[valueId] = stencils[i]
-        gradient += function(*argsList) * coefs[i]
-
-    gradient /= STENCIL_SIZE
-    return gradient
+ACCURACY_ORDER = 2 # Accuracy Order can be 2, 4, 6
 
 def numericalJacobian(function, argumentId, *args):
     '''
-    This function returns a jacobian matrix with the following dimension :
+    Returns a jacobian matrix of dimension :
     [function codomain dimension, function domain dimension]
     'Function codomain dimension' : dimension of the function output
     'Function domain dimension' : dimension of the input argumentId of the function
     Warning : Only use scalar as argument (no integer/boolean)
-    Note : This function can be used to compute the gradient of function\
+    Note : This function can be used to compute the gradient of function
     '''
     functionCodomainDimension = 1 # default when np.isscalar(args[argumentId])
     functionDomainDimension = 1 # default when np.isscalar(gradientList[0])
     gradientList = []
 
-    # compute gradients for each component of the argument
-    if not np.isscalar(args[argumentId]):
-        functionDomainDimension = len(args[argumentId])
+    # Differentiation settings
+    accuracyOrderIndex = (int)(ACCURACY_ORDER / 2) - 1
+    offsets = FIRST_DERIVATIVE_OFFSET[accuracyOrderIndex]
+    coefs = FIRST_DERIVATIVE_COEFS[accuracyOrderIndex]
 
-    for componentId in range(functionDomainDimension):
-        gradient = numericalDifferentiation(function, argumentId, componentId, *args)
+    # Copy the arguments of the function
+    # This is a shallow copy by consequence the numerical differientiation
+    # will have to do a deep copy for every modified argument
+    argsList = np.array(args)
+
+    if np.isscalar(args[argumentId]):
+        functionDomainDimension = 1
+        # deepcopy of the argument to be modified
+        argsList[argumentId] = np.copy(args[argumentId])
+
+        stencils = offsets + argsList[argumentId]
+
+        argsList[argumentId] = stencils[0]
+        gradient = function(*argsList) * coefs[0]
+        for i in range(1, len(stencils)):
+            argsList[argumentId] = stencils[i]
+            gradient += function(*argsList) * coefs[i]
+
+        gradient /= STENCIL_SIZE
         gradientList.append(gradient)
+    else:
+        functionDomainDimension = len(args[argumentId])
+        for componentId in range(functionDomainDimension):
+            # deepcopy of the argument to be modified
+            argsList[argumentId] = np.copy(args[argumentId])
 
-    # each gradient is a single value -> return a gradient
+            stencils = offsets + argsList[argumentId][componentId]
+
+            argsList[argumentId][componentId] = stencils[0]
+            gradient = function(*argsList) * coefs[0]
+            for i in range(1, len(stencils)):
+                argsList[argumentId][componentId] = stencils[i]
+                gradient += function(*argsList) * coefs[i]
+
+            gradient /= STENCIL_SIZE
+            gradientList.append(gradient)
+
+    # assemble jacobian/gradient from gradients
     if np.isscalar(gradientList[0]):
         gradient = np.asarray(gradientList)
         return gradient
 
-    # assemble jacobian from gradients
     functionCodomainDimension = len(gradientList[0])
     jacobian = np.zeros(shape=(functionCodomainDimension, functionDomainDimension))
     for gradientId, gradient in enumerate(gradientList):
@@ -91,7 +90,7 @@ def numericalJacobian(function, argumentId, *args):
 
 def numericalHessian(function, argumentId0, argumentId1, *args):
     '''
-    Return the Hessian by using two consecutive derivations (mixed derivatives)
+    Returns the Hessian by using two consecutive derivations (mixed derivatives)
     The order of differientation doesn't matter : see Clairaut's theorem/Schwarz's theorem
     '''
     class diffHelper:
