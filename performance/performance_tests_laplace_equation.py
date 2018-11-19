e513a4541aa0588d96e06233e78b4eca72942dc0
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2018-11-19 19:33:39 +1300

    refactor performance_tests_laplace_equation

--- performance/performance_tests_laplace_equation.py
+++ performance/performance_tests_laplace_equation.py
@@ -3,15 +3,26 @@
 @description : Evaluate CPU and stuff
 """
 
+import time
 import numpy as np
 from numba import njit, cuda, vectorize
-import numba
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+'''
+ Global Parameters
+'''
+NUM_NODES = 50
+JACOBI_ITERATIONS = 500
+
+# TODO - add time + graph
+# TODO - add numba cuda
 # TODO - Test Image restoration using partial differential equations
 
-def create_domain(num_nodes=50):
+def create_domain(num_nodes=64):
+    '''
+    Returns domain nodes along x, y and grid values
+    '''
     domain_x = np.linspace(0, 10, num=num_nodes, endpoint=True)
     domain_y = np.linspace(0, 10, num=num_nodes, endpoint=True)
 
@@ -20,7 +31,8 @@ def create_domain(num_nodes=50):
 
     return domain_x, domain_y, values
 
-def solve_laplace_equation_with_jacobi(x, num_iterations = 500):
+
+def jacobi_solver(x, next_x,  num_iterations=500):
     '''
     Jacobi method solves Ax=b where
     A contains the coefficient of the discrete laplace operator
@@ -36,23 +48,48 @@ def solve_laplace_equation_with_jacobi(x, num_iterations = 500):
     and aij are -1 for j!=i
     => xi(k+1) = 1/4 * (sum(xj(k)) 'for j!=i')
     '''
-    next_x = np.copy(x)
-
     num_dof_x = x.shape[0]
     num_dof_y = x.shape[1]
 
-    for iteration in range(1, num_iterations):
-        for i in range(1, num_dof_x - 1):
-            for j in range(1, num_dof_y - 1):
+    for iteration in range(num_iterations):
+        for j in range(1, num_dof_y - 1):
+            for i in range(1, num_dof_x - 1):
                 next_x[i][j] = (x[i-1][j] + x[i+1][j] + x[i][j-1] + x[i][j+1]) * 0.25
 
         np.copyto(x, next_x)
 
-# Execute and display
-domain_x, domain_y, values = create_domain()
-solve_laplace_equation_with_jacobi(values)
+@njit
+def numba_jacobi_solver(x, next_x,  num_iterations = 500):
+    num_dof_x = x.shape[0]
+    num_dof_y = x.shape[1]
 
+    for iteration in range(num_iterations):
+        for j in range(1, num_dof_y - 1):
+            for i in range(1, num_dof_x - 1):
+                next_x[i][j] = (x[i-1][j] + x[i+1][j] + x[i][j-1] + x[i][j+1]) * 0.25
+
+        # numba doesn't support copyto
+        #np.copyto(x, next_x)
+        for j in range(1, num_dof_y - 1):
+            for i in range(1, num_dof_x - 1):
+                x[i][j] = next_x[i][j]
+
+def solve_laplace_equation(x, algo = jacobi_solver, num_iterations = 500):
+    next_x = np.copy(x)
+    for iteration in range(0, num_iterations, 1):
+        algo(x, next_x, 1)
+
+# Execute and display
 fig, ax = plt.subplots()
+domain_x, domain_y, values = create_domain(NUM_NODES)
+
+start_time = time.time()
+solve_laplace_equation(values, numba_jacobi_solver, JACOBI_ITERATIONS)
+end_time = time.time()
+log = ' %f sec' % (end_time - start_time)
+print(log)
+
 im = ax.pcolormesh(domain_x, domain_y, values, cmap="rainbow", antialiased=True, shading="gouraud")
 fig.colorbar(im)
+#fig.savefig("test.png")
 
