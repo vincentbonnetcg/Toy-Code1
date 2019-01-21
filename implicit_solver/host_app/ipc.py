d36f16103408825881042bc574a697552a8f73e5
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-01-21 21:43:37 +1300

    add BundleIPC for common logic between client and server

--- implicit_solver/host_app/ipc.py
+++ implicit_solver/host_app/ipc.py
@@ -3,55 +3,82 @@
 @description : Inter-process communication between client and server via a socket
 """
 
-class Client:
+class BundleIPC:
     '''
-    Client stores the data and solver informations
+    Bundle is simply a container with a scene, a solver and a context
+    BundleIPC provides common behaviour between client and server
     '''
     def __init__(self, scene = None, solver = None, context = None):
-        # Socket Informations (TODO - future work)
-        self.host = "127.0.0.1"
-        self.port = 5050
-        # Data and Solver
         self.scene = scene
         self.solver = solver
         self.context = context
 
-    def connect_to_external_server(self):
-        # TODO : connect to a solver from another process
-        pass
-
-    def is_local(self):
+    def is_defined(self):
         if self.scene and self.solver and self.context:
             return True
-
         return False
 
     def initialize(self):
-        if self.is_local():
+        if self.is_defined():
             self.solver.initialize(self.scene, self.context)
-        else:
+            return True
+        return False
+
+    def step(self):
+        if self.is_defined():
+            self.context.time += self.context.dt
+            self.solver.solveStep(self.scene, self.context)
+            return True
+        return False
+
+    def get_scene(self):
+        return self.scene
+
+    def get_solver(self):
+        return self.solver
+
+class Client(BundleIPC):
+    '''
+    Client can store a bundle and/or get bundle from server
+    '''
+    def __init__(self, scene = None, solver = None, context = None):
+        BundleIPC.__init__(self, scene, solver, context)
+
+    def connect_to_external_server(self, host = "127.0.0.1", port = 5050):
+        # TODO : connect to a solver from another process
+        pass
+
+    def initialize(self):
+        if not super().initialize():
             # TODO : send request to server
             pass
 
     def step(self):
-        if self.is_local():
-            self.context.time += self.context.dt
-            self.solver.solveStep(self.scene, self.context)
-        else:
+        if not super().step():
             # TODO : send request to server
             pass
 
     def get_scene(self):
-        if self.is_local():
+        if super().is_defined():
             return self.scene
 
         # TODO : send request to server
         return None
 
     def get_solver(self):
-        if self.is_local():
+        if super().is_defined():
             return self.solver
 
         # TODO : send request to server
         return None
+
+class Server(BundleIPC):
+    '''
+    Server stores the data and solver informations
+    '''
+    def __init__(self, scene = None, solver = None, context = None):
+        BundleIPC.__init__(self, scene, solver, context)
+
+    def create_server(self, host = "127.0.0.1", port = 5050):
+        pass
 
