d076139cc68b8802a7e3d1388759358ff720122d
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-05-31 11:15:34 +1200

    add typing on commands.py

--- implicit_solver/tests/commands.py
+++ implicit_solver/tests/commands.py
@@ -4,38 +4,39 @@
 """
 
 import tests
+from objects import Condition, Force
 
-def add_wire_bending_constraint(scene, dynamic, stiffness, damping):
+def add_wire_bending_constraint(scene, dynamic, stiffness, damping) -> Condition:
     condition = tests.WireBendingCondition([dynamic], stiffness, damping)
     scene.add_condition(condition)
     return condition
 
-def add_edge_constraint(scene, dynamic, stiffness, damping):
+def add_edge_constraint(scene, dynamic, stiffness, damping) -> Condition:
     condition = tests.EdgeCondition([dynamic], stiffness, damping)
     scene.add_condition(condition)
     return condition
 
-def add_face_constraint(scene, dynamic, stiffness, damping):
+def add_face_constraint(scene, dynamic, stiffness, damping) -> Condition:
     condition = tests.AreaCondition([dynamic], stiffness, damping)
     scene.add_condition(condition)
     return condition
 
-def add_kinematic_attachment(scene, dynamic, kinematic, stiffness, damping, distance):
+def add_kinematic_attachment(scene, dynamic, kinematic, stiffness, damping, distance) -> Condition:
     condition = tests.KinematicAttachmentCondition(dynamic, kinematic, stiffness, damping, distance)
     scene.add_condition(condition)
     return condition
 
-def add_dynamic_attachment(scene, dynamic0, dynamic1, stiffness, damping, distance):
+def add_dynamic_attachment(scene, dynamic0, dynamic1, stiffness, damping, distance) -> Condition:
     condition = tests.DynamicAttachmentCondition(dynamic0, dynamic1, stiffness, damping, distance)
     scene.add_condition(condition)
     return condition
 
-def add_kinematic_collision(scene, dynamic, kinematic, stiffness, damping):
+def add_kinematic_collision(scene, dynamic, kinematic, stiffness, damping) -> Condition:
     condition = tests.KinematicCollisionCondition(dynamic, kinematic, stiffness, damping)
     scene.add_condition(condition)
     return condition
 
-def add_gravity(scene, gravity):
+def add_gravity(scene, gravity) -> Force:
     force = tests.Gravity(gravity)
     scene.add_force(force)
     return force
