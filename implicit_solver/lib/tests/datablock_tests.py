1e493928e0bed7b5e2c1745c757aca0cfcff69ab
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-07-19 10:51:39 +1200

    add set value test and numba_helper in datablock

--- implicit_solver/lib/tests/datablock_tests.py
+++ implicit_solver/lib/tests/datablock_tests.py
@@ -11,19 +11,52 @@ from numba import njit, vectorize
 '''
 Utilities
 '''
-class ClassType:
+def numba_helper(method):
+    '''
+    Decorator to replace common.DataBlock arguments, their respectives numpy array
+    '''
+    def execute(*args):
+        arg_list = list(args)
+        for arg_id , arg in enumerate(arg_list):
+            if (isinstance(arg, common.DataBlock)):
+                arg_list[arg_id] = arg.data
+
+        result = method(*arg_list)
+        return result
+
+    return execute
+
+'''
+Datablock Functions
+'''
+class ComponentTest:
 
     def __init__(self):
         self.field_0 = np.float64(0.6)
         self.field_1 = np.ones((2, 2), dtype = np.int64) * 0.5
 
-def create_test_datablock():
+# TODO - placeholder for vectorize test
+#def set_component(component : ComponentTest, value0, value1):
+#    component.field_0 = value0
+#    component.field_1 = value1
+
+def create_datablock():
     datablock = common.DataBlock()
     datablock.add_field('field_a', np.float64, 1)
     datablock.add_field('field_b', np.float64, (2, 2))
-    datablock.add_field_from_class(ClassType)
+    datablock.add_field_from_class(ComponentTest)
     datablock.initialize(10)
     return datablock
+
+@numba_helper
+@njit
+def set_datablock_values(datablock, value0, value1):
+    datablock_field_0 = datablock.field_0
+    datablock_field_1 = datablock.field_1
+
+    for i in range(len(datablock_field_0)):
+        datablock_field_0[i] = value0
+        datablock_field_1[i] = value1
 
 '''
 Datablock Tests
@@ -31,7 +64,7 @@ Datablock Tests
 class TestDataBlock(unittest.TestCase):
 
     def test_datablock_memory_datatype(self):
-        datablock = create_test_datablock()
+        datablock = create_datablock()
         datablock_type = np.dtype(datablock.data)
         self.assertEqual('field_a' in datablock_type.names, True)
         self.assertEqual('field_b' in datablock_type.names, True)
@@ -42,15 +75,22 @@ class TestDataBlock(unittest.TestCase):
         self.assertEqual(datablock_type.itemsize, 800)
 
     def test_datablock_memory_value(self):
-        datablock = create_test_datablock()
+        datablock = create_datablock()
         datablock.initialize(10)
         self.assertEqual(datablock.field_a[0], 0.0)
-        self.assertEqual(datablock.field_1[0][0][0], 0.5)
         self.assertEqual(datablock.field_0[0], 0.6)
+        self.assertEqual(datablock.field_1[0][0][0], 0.5)
+
+    def test_datablock_set_values(self):
+        datablock = create_datablock()
+        datablock.initialize(10)
+        set_datablock_values(datablock, 1.5, 2.5)
+        self.assertEqual(datablock.field_0[0], 1.5)
+        self.assertEqual(datablock.field_1[0][0][0], 2.5)
 
     @unittest.skip("skip test_datablock_vectorize")
     def test_datablock_vectorize(self):
-        datablock = create_test_datablock()
+        datablock = create_datablock()
         # TODO - placeholder for field iterator
         self.assertEqual(True, False)
 
