20244c90fcf0d5cedefbeed6e16be2d260cedc3c
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-07-27 11:31:19 +1200

    early code to generate function

--- implicit_solver/lib/tests/code_gen_tests.py
+++ implicit_solver/lib/tests/code_gen_tests.py
@@ -39,20 +39,49 @@ def test_vectorize(method):
             function_code = inspect.getsource(method)
 
             # check arguments
+            list_variable_names = []
+            list_variable_accessors = []
             function_args = inspect.signature(method)
             for param in function_args.parameters:
                 # regular expression to check whether the argument is an object with attribute
                 param_attrs = re.findall(param+'[.][a-zA-Z0-9_]*', function_code)
-                print(param_attrs)
+                for param_attr in param_attrs:
+                    # object.attr => object_attr (variable name)
+                    variable_name = param_attr.replace('.', '_')
+                    # object.attr into object['attr'] (variable accessor)
+                    object_attr = param_attr.split('.')
+                    variable_accessor = object_attr[0] +'[\'' + object_attr[1] + '\']'
+                    # add to list
+                    list_variable_names.append(variable_name)
+                    list_variable_accessors.append(variable_accessor)
 
-            # remove any decorators from the function
+            # generate code
             code_lines = function_code.splitlines()
             new_code_lines = []
             for code in code_lines:
+                # remove any decorators from the function
                 if code[0] != '@':
                     new_code_lines.append(code)
+
+                if code[0:4] == 'def ':
+                    # TODO - add suffix to
+                    num_variables = len(list_variable_names)
+                    for var_id in range(num_variables):
+                        variable_name = list_variable_names[var_id]
+                        variable_accessor = list_variable_accessors[var_id]
+                        # TODO - need to improve the tabulation
+                        variable_code = '    ' + variable_name + ' = ' + variable_accessor
+                        new_code_lines.append(variable_code)
+                        # TODO - add loop
+                else:
+                    # TODO replace object.attr with object_attr[i]
+                    pass
+
+            # generate code
             function_code = '\n'.join(new_code_lines)
+            print(" --- generated code --- ")
             print(function_code)
+            #TODO - exec(function_code)
 
         # TODO - call the generated function
         #generated_function_ptr(*arg_list)
@@ -64,7 +93,7 @@ def test_vectorize(method):
 
 @test_vectorize
 def test_add(v0 : Vertex, v1 : Vertex, other_value):
-    v0.x += v1.xy5_8 + other_value
+    v0.x += v1.x + other_value
     v0.y += v1.y + other_value
 
 @numba_friendly
