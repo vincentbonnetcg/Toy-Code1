6aaa7d26feae46b31e0484047b96df84375d8347
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-09-04 20:24:37 +1200

    vectorize optimal transformation

--- optimalTransformation_covarianceMatrix.py
+++ optimalTransformation_covarianceMatrix.py
@@ -15,47 +15,47 @@ SCALE = [12.0, 4.0]
 TRANSLATE = [2.5, 1]
 
 '''
- Point Data
-'''
-class PointData:
-        def __init__(self):
-            self.x = np.array(np.random.rand(NUM_POINTS, 2) - 0.5)  # positions [-0.5, -0.5] [0.5, 0.5]
-
-''' 
  Transform point
 '''
 def transformPoints(pointData, angle, scale, translate):
     cosAngle = np.cos(np.deg2rad(angle))
     sinAngle = np.sin(np.deg2rad(angle))
-    
+
     # create scale-rotation matrix
-    scaleRotationMax = np.matrix([[cosAngle * scale[0],  -sinAngle * scale[1]],
+    scaleRotationMat = np.matrix([[cosAngle * scale[0],  -sinAngle * scale[1]],
                                   [sinAngle * scale[0],  cosAngle * scale[1]]])
-    
-    # translate points
-    for i in range(NUM_POINTS): 
-        vec = np.matmul(scaleRotationMax, pointData.x[i])
-        vec += translate
-        pointData.x[i]= vec
+
+    # Explicit
+    #for i in range(NUM_POINTS):
+    #    pointData[i] = np.matmul(scaleRotationMat, pointData[i])
+    #    pointData[i] += translate
+
+    # Vectorized
+    multiply = np.matmul(scaleRotationMat, pointData.T)
+    np.copyto(pointData, multiply.T)
+    pointData += translate
+
 
 '''
  Utility functions
-''' 
-def numElements(pointData):
-    return np.size(pointData.x,0)
-
+'''
 def computeCentroid(pointData):
-    return np.sum(pointData.x, axis=0) / numElements(pointData)
+    return np.sum(pointData, axis=0) / np.size(pointData,0)
 
 def covariance(pointArray, index0, index1):
-    cov = 0
-    for p in pointArray:
-        cov += p[index0] * p[index1]
-    return cov / (np.size(pointArray,0))
+    # Explicit
+    #cov = 0
+    #for p in pointArray:
+    #    cov += p[index0] * p[index1]
+
+    # Vectorized
+    cov = np.sum(pointArray[:,index0] * pointArray[:,index1])
+
+    return cov / np.size(pointArray,0)
 
 def computeOrthogonalFrame(pointData, centroid):
     # get direction
-    localPoint = np.copy(pointData.x)  
+    localPoint = np.copy(pointData)
     np.subtract(localPoint, centroid, out=localPoint)
     # compute covariance matrix
     # cov(X,X) cov(X,Y)
@@ -79,34 +79,34 @@ def drawPointsAndFrame(pointData):
     ax.set_aspect('equal')
     ax.set_xlim(-10,10)
     ax.set_ylim(-10,10)
-    
+
     # compute eigenvectors
     centroid = computeCentroid(pointData)
     frames = computeOrthogonalFrame(pointData, centroid)
-    
+
     vec0 = np.reshape(frames[0], (2, 1))
     vec1 = np.reshape(frames[1], (2, 1))
     vec0x = np.asscalar(vec0[0])
     vec0y = np.asscalar(vec0[1])
     vec1x = np.asscalar(vec1[0])
     vec1y = np.asscalar(vec1[1])
-    
+
     # draw the local frame
     # in ax.arrow(...)  vec.y and vec.x are reversed !
     ax.arrow(centroid[0], centroid[1], vec0y, vec0x, head_width=1.0, facecolor='red', edgecolor='black')
     ax.arrow(centroid[0], centroid[1], vec1y, vec1x, head_width=1.0, facecolor='green', edgecolor='black')
-    
+
     # draw points
-    x, y = zip(*pointData.x)
+    x, y = zip(*pointData)
     ax.scatter(x, y,s=0.5)
-    
+
     # show result
     plt.show()
 
 '''
  Execute
 '''
-pointData = PointData()
+pointData = np.array(np.random.rand(NUM_POINTS, 2) - 0.5)  # positions [-0.5, -0.5] [0.5, 0.5]
 transformPoints(pointData, ROTATION, SCALE, TRANSLATE)
 
 drawPointsAndFrame(pointData)
