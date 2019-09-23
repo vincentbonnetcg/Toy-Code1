53ac5fabc434d160d890746a3b11be167408f5f7
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-09-23 15:16:01 +1200

    update area to support blocks

--- implicit_solver/lib/objects/components/constraints/area.py
+++ implicit_solver/lib/objects/components/constraints/area.py
@@ -35,44 +35,51 @@ class Area(ConstraintBase):
         '''
         Add the force to the datablock
         '''
-        node_ids_ptr = datablock_cts.node_ids
-        rest_area_ptr = datablock_cts.rest_area
-        stiffness_ptr = datablock_cts.stiffness
-        #damping_ptr = datablock_cts.damping  # NOT IMPLEMENTED YET
-        force_ptr = datablock_cts.f
+        scene.update_blocks_from_data()
 
-        for ct_index in range(len(datablock_cts)):
-            x0, v0 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][0])
-            x1, v1 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][1])
-            x2, v2 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][2])
-            f0, f1, f2 = elastic_area_forces(x0, x1, x2, rest_area_ptr[ct_index], stiffness_ptr[ct_index], (True, True, True))
-            force_ptr[ct_index][0] = f0
-            force_ptr[ct_index][1] = f1
-            force_ptr[ct_index][2] = f2
+        for ct_block in datablock_cts.blocks:
+            node_ids_ptr = ct_block['node_ids']
+            rest_area_ptr = ct_block['rest_area']
+            stiffness_ptr = ct_block['stiffness']
+            force_ptr = ct_block['f']
+
+            for ct_index in range(len(datablock_cts)):
+                x0, v0 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][0])
+                x1, v1 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][1])
+                x2, v2 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][2])
+                f0, f1, f2 = elastic_area_forces(x0, x1, x2, rest_area_ptr[ct_index], stiffness_ptr[ct_index], (True, True, True))
+                force_ptr[ct_index][0] = f0
+                force_ptr[ct_index][1] = f1
+                force_ptr[ct_index][2] = f2
+
+        scene.update_data_from_blocks()
 
     @classmethod
     def compute_jacobians(cls, datablock_cts : DataBlock, scene : Scene) -> None:
         '''
         Add the force jacobian functions to the datablock
         '''
-        node_ids_ptr = datablock_cts.node_ids
-        rest_area_ptr = datablock_cts.rest_area
-        stiffness_ptr = datablock_cts.stiffness
-        #damping_ptr = datablock_cts.damping # NOT IMPLEMENTED YET
-        dfdx_ptr = datablock_cts.dfdx
-        #dfdv_ptr = datablock_cts.dfdv # NOT IMPLEMENTED YET
+        scene.update_blocks_from_data()
 
-        for ct_index in range(len(datablock_cts)):
-            x0, v0 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][0])
-            x1, v1 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][1])
-            x2, v2 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][2])
-            dfdx = elastic_area_numerical_jacobians(x0, x1, x2, rest_area_ptr[ct_index], stiffness_ptr[ct_index])
-            dfdx_ptr[ct_index][0][0] = dfdx[0]
-            dfdx_ptr[ct_index][1][1] = dfdx[1]
-            dfdx_ptr[ct_index][2][2] = dfdx[2]
-            dfdx_ptr[ct_index][0][1] = dfdx_ptr[ct_index][1][0] = dfdx[3]
-            dfdx_ptr[ct_index][0][2] = dfdx_ptr[ct_index][2][0] = dfdx[4]
-            dfdx_ptr[ct_index][1][2] = dfdx_ptr[ct_index][2][1] = dfdx[5]
+        for ct_block in datablock_cts.blocks:
+            node_ids_ptr = ct_block['node_ids']
+            rest_area_ptr = ct_block['rest_area']
+            stiffness_ptr = ct_block['stiffness']
+            dfdx_ptr = ct_block['dfdx']
+
+            for ct_index in range(len(datablock_cts)):
+                x0, v0 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][0])
+                x1, v1 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][1])
+                x2, v2 = na.node_xv(scene.dynamics, node_ids_ptr[ct_index][2])
+                dfdx = elastic_area_numerical_jacobians(x0, x1, x2, rest_area_ptr[ct_index], stiffness_ptr[ct_index])
+                dfdx_ptr[ct_index][0][0] = dfdx[0]
+                dfdx_ptr[ct_index][1][1] = dfdx[1]
+                dfdx_ptr[ct_index][2][2] = dfdx[2]
+                dfdx_ptr[ct_index][0][1] = dfdx_ptr[ct_index][1][0] = dfdx[3]
+                dfdx_ptr[ct_index][0][2] = dfdx_ptr[ct_index][2][0] = dfdx[4]
+                dfdx_ptr[ct_index][1][2] = dfdx_ptr[ct_index][2][1] = dfdx[5]
+
+        scene.update_data_from_blocks()
 
 '''
  Utility Functions
