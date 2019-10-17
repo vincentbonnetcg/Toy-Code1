4514fb78cda398a799c35f11b61cdd59fcedfd6d
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2019-10-17 20:57:33 +1300

    update demo_march_2019

--- implicit_solver/logic/demos/demo_march_2019.py
+++ implicit_solver/logic/demos/demo_march_2019.py
@@ -2,11 +2,11 @@
 @author: Vincent Bonnet
 @description : Demo file to init the solver
 """
-import system
-from host_app.client import Client
+import lib
+import host_app.rpc as rpc
 from host_app.Ipython.render import Render
 from logic import scene_examples, RectangleShape
-import objects
+
 import time
 
 '''
@@ -19,12 +19,12 @@ NUM_FRAMES = 100 # number of simulated frame (doesn't include initial frame)
 
 g_render = Render()
 g_frame_id = 0
-context = system.Context(time = START_TIME, frame_dt = FRAME_TIMESTEP,
+context = lib.system.Context(time = START_TIME, frame_dt = FRAME_TIMESTEP,
                      num_substep = NUM_SUBSTEP, num_frames = NUM_FRAMES)
 
 def get_dispatcher(name):
-    cmd_dispatcher = Client(name)
-    if not cmd_dispatcher.connect_to_server(ip='127.0.0.1', port=8080):
+    cmd_dispatcher = rpc.Client(name)
+    if not cmd_dispatcher.connect_to_server(ip='127.0.0.1', port=8013):
         print('Cannot connect to server')
     return cmd_dispatcher
 
@@ -35,7 +35,7 @@ def stage_init_wire_scene(client_name):
     cmd_dispatcher = get_dispatcher(client_name)
 
     cmd_dispatcher.run('set_context', context = context)
-    scene_examples.init_wire_example(cmd_dispatcher)
+    scene_examples.init_wire_example(cmd_dispatcher, g_render)
 
     cmd_dispatcher.run('initialize')
     g_render.show_current_frame(cmd_dispatcher, g_frame_id)
@@ -60,7 +60,7 @@ def stage_add_collider(client_name):
     func = lambda time: [[rectangle_position[0],
                           rectangle_position[1]], -5.0]
 
-    rectangle_animator = objects.Animator(func, context)
+    rectangle_animator = lib.objects.Animator(func, context)
 
     rectangle_handle = cmd_dispatcher.run('add_kinematic', shape = rectangle_shape,
                                                           position = rectangle_position,
