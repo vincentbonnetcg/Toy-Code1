b9a63dd55bda12258178be4f40a19b98feaf1579
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-02-10 20:35:13 +1300

    move sparse matrix in experiments

--- implicit_solver/lib/common/__init__.py
+++ implicit_solver/lib/common/__init__.py
@@ -4,4 +4,3 @@ from lib.common.shape import Shape
 from lib.common.convex_hull import ConvexHull
 from lib.common.data_block import DataBlock
 from lib.common.profiler import Profiler, timeit
-from lib.common.sparse_matrix import BSRSparseMatrix
