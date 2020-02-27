9ef152f8c82b78dd7738aa55b8836e3adb2f8625
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-02-28 09:53:36 +1300

    covarianceMatrix

--- miscellaneous/optimalTransformation_covarianceMatrix.py
+++ miscellaneous/optimalTransformation_covarianceMatrix.py
@@ -5,13 +5,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 '''
  Global Constants
 '''
 NUM_POINTS = 1000
 ROTATION = 39.0
-SCALE = [12.0, 4.0]
+SCALE = [11.0, 4.0]
 TRANSLATE = [2.5, 1]
 
 '''
@@ -25,16 +26,14 @@ def transformPoints(pointData, angle, scale, translate):
     scaleRotationMat = np.matrix([[cosAngle * scale[0],  -sinAngle * scale[1]],
                                   [sinAngle * scale[0],  cosAngle * scale[1]]])
 
-    # Explicit
+    # Non vectorized code
     #for i in range(NUM_POINTS):
     #    pointData[i] = np.matmul(scaleRotationMat, pointData[i])
     #    pointData[i] += translate
 
-    # Vectorized
-    multiply = np.matmul(scaleRotationMat, pointData.T)
-    np.copyto(pointData, multiply.T)
+    transformedPoint = np.matmul(scaleRotationMat, pointData.T)
+    np.copyto(pointData, transformedPoint.T)
     pointData += translate
-
 
 '''
  Utility functions
@@ -43,12 +42,11 @@ def computeCentroid(pointData):
     return np.sum(pointData, axis=0) / np.size(pointData,0)
 
 def covariance(pointArray, index0, index1):
-    # Explicit
+    # Non vectorized code
     #cov = 0
     #for p in pointArray:
     #    cov += p[index0] * p[index1]
 
-    # Vectorized
     cov = np.sum(pointArray[:,index0] * pointArray[:,index1])
 
     return cov / np.size(pointArray,0)
@@ -69,36 +67,43 @@ def computeOrthogonalFrame(pointData, centroid):
     w, v = np.linalg.eig(covarianceMatrix)
     return v
 
-'''
- Draw point data and frame
-'''
-def drawPointsAndFrame(pointData):
-    # create figure
+def draw(pointData, centroid, rotationMatrix):
+    '''
+     Draw point, frames, box
+    '''
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_aspect('equal')
     ax.set_xlim(-10,10)
     ax.set_ylim(-10,10)
 
-    # compute eigenvectors
-    centroid = computeCentroid(pointData)
-    frames = computeOrthogonalFrame(pointData, centroid)
-
-    vec0 = np.reshape(frames[0], (2, 1))
-    vec1 = np.reshape(frames[1], (2, 1))
-    vec0x = vec0[0].item()
-    vec0y = vec0[1].item()
-    vec1x = vec1[0].item()
-    vec1y = vec1[1].item()
+    transformedPointData = np.copy(pointData)
+    transformedPointData -= centroid
+    inverseRotationMatrix = rotationMatrix.T
+    transformedPointData = np.asarray(np.matmul(inverseRotationMatrix, transformedPointData.T).T)
+    boxMin = np.min(transformedPointData, axis=0)
+    boxMax = np.max(transformedPointData, axis=0)
 
     # draw the local frame
     # in ax.arrow(...)  vec.y and vec.x are reversed !
+    vec0x = frames.item(0,0)
+    vec0y = frames.item(0,1)
+    vec1x = frames.item(1,0)
+    vec1y = frames.item(1,1)
     ax.arrow(centroid[0], centroid[1], vec0y, vec0x, head_width=1.0, facecolor='red', edgecolor='black')
     ax.arrow(centroid[0], centroid[1], vec1y, vec1x, head_width=1.0, facecolor='green', edgecolor='black')
 
     # draw points
     x, y = zip(*pointData)
     ax.scatter(x, y,s=0.5)
+
+    # draw box
+    vertex = np.asarray([(boxMin[0], boxMin[1]), (boxMin[0], boxMax[1]),
+                         (boxMax[0], boxMax[1]), (boxMax[0], boxMin[1])])
+    vertex = np.asarray(np.matmul(rotationMatrix, vertex.T).T)
+    vertex += centroid
+    polygon = patches.Polygon(vertex, closed=True,ec='darkgreen',lw=2,fill=False)
+    ax.add_patch(polygon)
 
     # show result
     plt.show()
@@ -109,4 +114,13 @@ def drawPointsAndFrame(pointData):
 pointData = np.array(np.random.rand(NUM_POINTS, 2) - 0.5)  # positions [-0.5, -0.5] [0.5, 0.5]
 transformPoints(pointData, ROTATION, SCALE, TRANSLATE)
 
-drawPointsAndFrame(pointData)
+# compute best transform (rotation, centroid)
+centroid = computeCentroid(pointData)
+frames = computeOrthogonalFrame(pointData, centroid)
+det = np.linalg.det(frames)
+if det < 0.0:
+    frames *= -1.0
+rotationMatrix = frames
+
+# draw
+draw(pointData, centroid, rotationMatrix)
