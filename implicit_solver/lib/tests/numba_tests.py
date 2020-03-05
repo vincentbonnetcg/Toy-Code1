fd6477609d83d4b46d25431c5d2c05db3b186ca4
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-03-05 16:44:28 +1300

    add dummy/inactive block in numba_tests

--- implicit_solver/lib/tests/numba_tests.py
+++ implicit_solver/lib/tests/numba_tests.py
@@ -15,26 +15,35 @@ class ComponentTest:
         self.field_1 = np.ones((2, 2), dtype = np.int64)
 
 def create_datablock(num_elements, block_size = 100):
-    datablock = common.DataBlock(ComponentTest, block_size, dummy_block=False)
+    datablock = common.DataBlock(ComponentTest, block_size, dummy_block=True)
     datablock.initialize(num_elements)
     return datablock
 
 @numba.njit
-def create_block_data(block_dtype, num_elements):
+def create_block_data(block_dtype, num_elements, active=True):
     block_data = np.zeros(1, dtype=block_dtype)
     item = block_data[0]
     item.field_0[:] = np.float64(0.5)
     item.field_1[:] = np.ones((2, 2), dtype = np.int64)
     item['blockInfo_numElements'] = num_elements
-    item['blockInfo_active'] = True
+    item['blockInfo_active'] = active
     return block_data
+
+@numba.njit
+def append_block_data(array, block_data):
+    array.append(block_data)
 
 @numba.njit
 def create_typed_list(block_dtype, num_blocks):
     array = numba.typed.List()
+    # add dummy/inactive block
+    block_data = create_block_data(block_dtype, num_elements=0, active=False)
+    array.append(block_data)
+
+    # add active blocks
     for _ in range(num_blocks):
         block_data = create_block_data(block_dtype, num_elements=10)
-        array.append(block_data)
+        append_block_data(array, block_data)
     return array
 
 class Tests(unittest.TestCase):
@@ -42,11 +51,17 @@ class Tests(unittest.TestCase):
         datablock = create_datablock(num_elements = 0, block_size = 100)
         block_dtype = datablock.get_block_dtype()
         blocks = create_typed_list(block_dtype, num_blocks = 15)
+        self.assertEqual(len(blocks), 16) # include inactive block
+        # Test dummy/inactive block
         block_data = blocks[0]
         item = block_data[0]
-        componentTest = ComponentTest()
+        self.assertEqual(item['blockInfo_numElements'], 0)
+        self.assertEqual(item['blockInfo_active'], False)
 
-        self.assertEqual(len(blocks), 15)
+        # Test first active block
+        block_data = blocks[1]
+        item = block_data[0]
+        componentTest = ComponentTest()
         self.assertEqual(item['blockInfo_numElements'], 10)
         self.assertEqual(item['blockInfo_active'], True)
         self.assertTrue(item['field_0'][0] == componentTest.field_0)
