c210e4235af82605d468adc2b77885544e83827f
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-03-08 16:55:00 +1300

    test njit create/set/append block

--- implicit_solver/lib/tests/numba_tests.py
+++ implicit_solver/lib/tests/numba_tests.py
@@ -14,42 +14,55 @@ class ComponentTest:
         self.field_0 = np.float64(0.5)
         self.field_1 = np.ones((2, 2), dtype = np.int64)
 
-def create_datablock(num_elements, block_size = 100):
+def get_block_dtype(block_size = 100):
     datablock = common.DataBlock(ComponentTest, block_size, dummy_block=True)
-    datablock.initialize(num_elements)
-    return datablock
+    return datablock.get_block_dtype()
 
 @numba.njit
-def create_block_data(block_dtype, num_elements, active=True):
+def create_block(block_dtype):
     block_data = np.zeros(1, dtype=block_dtype)
+    return block_data
+
+@numba.njit
+def set_block(block_data, num_elements, active=True):
     item = block_data[0]
     item.field_0[:] = np.float64(0.5)
     item.field_1[:] = np.ones((2, 2), dtype = np.int64)
     item['blockInfo_numElements'] = num_elements
     item['blockInfo_active'] = active
-    return block_data
 
 @numba.njit
-def append_block_data(array, block_data):
+def append_block(array, block_data):
     array.append(block_data)
+
+@numba.njit
+def get_inactive_block_indices(array):
+    indices = numba.typed.List.empty_list(numba.types.int64)
+    for block_index in range(len(array)):
+        item = array[block_index][0]
+        if not item['blockInfo_active']:
+            indices.append(block_index)
+    return indices
 
 @numba.njit
 def create_typed_list(block_dtype, num_blocks):
     array = numba.typed.List()
     # add dummy/inactive block
-    block_data = create_block_data(block_dtype, num_elements=0, active=False)
+    block_data = create_block(block_dtype)
+    set_block(block_data, num_elements=0, active=False)
     array.append(block_data)
 
     # add active blocks
     for _ in range(num_blocks):
-        block_data = create_block_data(block_dtype, num_elements=10)
-        append_block_data(array, block_data)
+        block_data = create_block(block_dtype)
+        set_block(block_data, num_elements=10, active=True)
+        append_block(array, block_data)
     return array
+
 
 class Tests(unittest.TestCase):
     def test_typed_list(self):
-        datablock = create_datablock(num_elements = 0, block_size = 100)
-        block_dtype = datablock.get_block_dtype()
+        block_dtype = get_block_dtype(block_size = 100)
         blocks = create_typed_list(block_dtype, num_blocks = 15)
         self.assertEqual(len(blocks), 16) # include inactive block
         # Test dummy/inactive block
@@ -66,6 +79,12 @@ class Tests(unittest.TestCase):
         self.assertEqual(item['blockInfo_active'], True)
         self.assertTrue(item['field_0'][0] == componentTest.field_0)
         self.assertTrue((item['field_1'][0] == componentTest.field_1).all())
+
+    def test_inactive(self):
+        block_dtype = get_block_dtype(block_size = 100)
+        blocks = create_typed_list(block_dtype, num_blocks = 15)
+        block_indices = get_inactive_block_indices(blocks)
+        self.assertTrue(len(block_indices) == 1)
 
     def setUp(self):
         print(" Numba Test:", self._testMethodName)
