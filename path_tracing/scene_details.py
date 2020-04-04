613134b769af297f66f5ef0ea1274273650ae78d
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-04-05 10:29:34 +1200

    placeholder for scene details

--- path_tracing/scene_details.py
+++ path_tracing/scene_details.py
@@ -21,7 +21,7 @@ class Sphere():
 
     def intersect(self, ray : jit_core.Ray):
         t = jit_maths.ray_sphere(ray.o, ray.d, self.c, self.r)
-        hit = jit_core. Hit(t)
+        hit = jit_core.Hit(t)
         if hit.valid():
             hit.p = ray.o + (ray.d * t)
             hit.n = (hit.p - self.c) / self.r
@@ -40,6 +40,18 @@ class PolygonSoup():
     def set_polygons(self, vertices, triangle_indices, normals):
         self.tv = np.take(vertices, triangle_indices, axis=0)
         self.n = normals
+
+    def get_triangles(self):
+        result = []
+        for triVertices in self.tv:
+            result.append(triVertices)
+        return result
+
+    def get_normals(self):
+        result = []
+        for triNormal in self.n:
+            result.append(triNormal)
+        return result
 
     def intersect(self, ray : jit_core.Ray):
         min_t = np.finfo(np.float).max
@@ -66,11 +78,11 @@ class Scene:
 
     def load(self):
         # create sphere
-        sphere_center = np.zeros(3)
-        np.copyto(sphere_center, [0, -0.1, -2])
-        sphere_radius = 0.5
-        sphere = Sphere(sphere_center, sphere_radius)
-        self.objects.append(sphere)
+        #sphere_center = np.zeros(3)
+        #np.copyto(sphere_center, [0, -0.1, -2])
+        #sphere_radius = 0.5
+        #sphere = Sphere(sphere_center, sphere_radius)
+        #self.objects.append(sphere)
         # create polygon soup
         polygon = PolygonSoup()
         self.objects.append(polygon)
@@ -85,3 +97,35 @@ class Scene:
             if (obj_hit.valid() and obj_hit.t < hit.t) or not hit.valid():
                 hit = obj_hit
         return hit
+
+    def details(self):
+        # gather sphere, triangles and materials
+        spheres = []
+        triangles = []
+        triangle_normals = []
+        triangle_materials = []
+        for obj in self.objects:
+            if isinstance(obj,Sphere):
+                spheres.append(obj)
+            elif isinstance(obj, PolygonSoup):
+                triangles += obj.get_triangles()
+                triangle_normals += obj.get_normals()
+                triangle_materials.append(obj.material.d)
+
+        # consolidate spheres in contiguous numpy array
+        # TODO
+
+        # consolidate triangles in contiguous numpy array
+        np_tri_vertices= np.zeros((len(triangles),3,3))
+        np_tri_normals = np.zeros((len(triangles),3))
+        np_tri_materials = np.zeros((len(triangles),3))
+        for ti in range(len(triangles)):
+            np_tri_vertices[ti] = triangles[ti]
+            np_tri_normals[ti] = triangle_normals[ti]
+            np_tri_materials[ti] = triangle_materials[ti]
+
+        print('num_spheres ' , len(spheres))
+        print('num_triangles ' , len(triangles))
+
+        details = (np_tri_vertices, np_tri_normals, np_tri_materials)
+        return details
