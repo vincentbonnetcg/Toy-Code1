ab53f429e17cc445de3016e17de1821870514dd6
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-10-08 08:49:36 +1300

    cleanup inverse jacobian

--- miscellaneous/inverseKinematics.py
+++ miscellaneous/inverseKinematics.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 NUM_SEGMENTS = 10
 INITIAL_ANGLE_RANGE = 0.0 #  [-angle, angle] in degrees for initial segment angle
 INITIAL_SEGMENT_LENGTH = 1000.0 # length each segment
-TARGET_POS = (-500.0, 2000.0)
+TARGET_POS = np.asarray([-500.0, 2000.0])
 
 REGULARIZATION_TERM = 0.1
 NUM_ITERATIONS = 100
@@ -23,28 +23,23 @@ class Chain:
     def __init__(self):
         self.angles = np.ones(NUM_SEGMENTS) * INITIAL_ANGLE_RANGE
         self.lengths = np.ones(NUM_SEGMENTS) * INITIAL_SEGMENT_LENGTH
-        self.numSegments = NUM_SEGMENTS
 
     def compute_positions(self):
-        rootPos = [0., 0.]
-        totalPos = rootPos
-        totalAngle = 0.
+        num_pos = len(self.angles)+1
+        positions = np.empty((num_pos, 2))
+        positions[0] = [0, 0]
+        angle = 90.
 
-        positions = []
-        positions.append(np.copy(rootPos))
-
-        for i in range(chain.numSegments):
-            totalAngle += chain.angles[i]
-            x = np.cos(np.deg2rad(totalAngle + 90)) * chain.lengths[i]
-            y = np.sin(np.deg2rad(totalAngle + 90)) * chain.lengths[i]
-            totalPos[0] += x
-            totalPos[1] += y
-            positions.append(np.copy(totalPos))
+        for i in range(NUM_SEGMENTS):
+            angle += chain.angles[i]
+            positions[i+1][0] = np.cos(np.deg2rad(angle))
+            positions[i+1][1] = np.sin(np.deg2rad(angle))
+            positions[i+1] *= chain.lengths[i]
+            positions[i+1] += positions[i]
 
         return positions
 
 '''
-# Jacobian Helper functions
 Layout of the partial differential matrix of size(m x n)
 'm' is the number of DOF for the end effector (only translate XY)
 'n' is the number of joints multiplied by the number of joint DOF
@@ -53,60 +48,38 @@ Layout of the partial differential matrix of size(m x n)
 x :  dx/da0     dx/da1     dx/da2
 y :  dy/da0     dy/da1     dy/da2
 '''
-#  Use central difference to approximate the differentiation
-def numericalJacobian(chain):
-    jacobian = np.zeros(shape=(2,chain.numSegments))
-    angleDt = 0.01
-    for i in range(chain.numSegments):
-        keepAngle = chain.angles[i]
-        # compute the derivative
-        chain.angles[i] = keepAngle + (angleDt * 0.5)
-        forwardPositions = chain.compute_positions()
-        chain.angles[i] = keepAngle - (angleDt * 0.5)
-        backwardPositions = chain.compute_positions()
-        dpda = (forwardPositions[chain.numSegments] - backwardPositions[chain.numSegments]) / angleDt
-        jacobian[0, i] = dpda[0]
-        jacobian[1, i] = dpda[1]
-        # resort angle
-        chain.angles[i] = keepAngle
-    return np.matrix(jacobian)
-
 def analyticJacobian(chain):
     positions = chain.compute_positions()
-    jacobian = np.zeros(shape=(2,chain.numSegments))
-    for i in range(chain.numSegments):
-        vec = np.subtract(positions[chain.numSegments],  positions[i])
-        x = vec[0]
-        y = vec[1]
-        # compute the derivative
-        # sin(np.deg2rad(1.0)) is the angular velocity
+    J = np.zeros(shape=(2,NUM_SEGMENTS))
+    for i in range(NUM_SEGMENTS):
+        vec = positions[-1] -  positions[i]
         # x = norm(vec) * cos(angle) and x' = norm(vec) * -sin(angle)
         # y = norm(vec) * sin(angle) and y' = norm(vec) * cos(angle)
         # hence x' = y * -1 and y' = x
-        jacobian[0, i] = y * -1 * np.sin(np.deg2rad(1.0))
-        jacobian[1, i] = x * np.sin(np.deg2rad(1.0))
+        J[0, i] = vec[1] * -1 * np.sin(np.deg2rad(1.0))
+        J[1, i] = vec[0] * np.sin(np.deg2rad(1.0))
 
-    return np.matrix(jacobian)
+    return np.matrix(J)
 
 '''
 Compute the Inverse of the Jacobians
 '''
-def dampedLeastSquare(jacobian):
+def dampedLeastSquare(J):
     damping_matrix_constant = np.identity(2) * REGULARIZATION_TERM
-    jacobiantInv = jacobian * jacobian.transpose()
-    jacobiantInv += damping_matrix_constant
-    jacobiantInv = np.linalg.inv(jacobiantInv)
-    return(jacobian.transpose() * jacobiantInv)
+    invJ = J * J.transpose()
+    invJ += damping_matrix_constant
+    invJ = np.linalg.inv(invJ)
+    return(J.transpose() * invJ)
 
 
 def printSingluarValues(matrix):
     '''
     Singular values analysis (for debugging)
     Print the singular values to indicate whether the matrix inversion is stable
-    Large singular values would make it unstable
-    matrix is decompose into U.E.Vt
+    SVD decompose the matrix into U.E.Vt
     where U and V are othogonal and E is the diagonal matrix containing singular values
     hence its inverse is V.1/E.Ut
+    Large singular values makes the matrix inverse going to infinity
     '''
     singular_values = np.linalg.svd(matrix, compute_uv = False)
     print("-- singular_values --")
@@ -118,29 +91,28 @@ def inverseKinematic(chain):
     Solve inverse kinematic problem
     '''
     positions = chain.compute_positions()
-    vec = np.subtract(TARGET_POS, positions[chain.numSegments])
-    vecNorm = np.linalg.norm(vec)
-    if (vecNorm > MAX_STEP_SIZE):
-        vec /= vecNorm
+    vec = np.reshape(TARGET_POS - positions[-1], (2,1))
+    vec_norm = np.linalg.norm(vec)
+    if (vec_norm > MAX_STEP_SIZE):
+        vec /= vec_norm
         vec *= MAX_STEP_SIZE
 
-    jacobian = analyticJacobian(chain)
-    pseudoInverse = dampedLeastSquare(jacobian)
+    J = analyticJacobian(chain)
+    pseudoInverse = dampedLeastSquare(J)
 
     # Debugging
     #printSingluarValues(pseudoInverse)
 
-    deltaAngles = np.matmul(pseudoInverse, np.reshape(vec, (2,1)))
-    for i in range(chain.numSegments):
-        chain.angles[i] += deltaAngles[i]
+    delta_angles = np.matmul(pseudoInverse, vec)
+    for i in range(NUM_SEGMENTS):
+        chain.angles[i] += delta_angles[i]
 
 def hasReachTarget(chain):
     '''
     Return true if an 'acceptable' solution has been reached
     '''
     positions = chain.compute_positions()
-    diff = np.subtract(TARGET_POS, positions[chain.numSegments])
-    if (np.linalg.norm(diff) <= THRESHOLD):
+    if (np.linalg.norm(TARGET_POS - positions[-1]) <= THRESHOLD):
         return True
     return False
 
