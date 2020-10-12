c4caaf2b5297651ce81f07ca88a3ba3750b24685
Vincent Bonnet <vincentbonnet@hotmail.co.uk>
2020-10-12 17:42:44 +1300

    cleanup

--- miscellaneous/inverseKinematics.py
+++ miscellaneous/inverseKinematics.py
@@ -1,19 +1,13 @@
 """
 @author: Vincent Bonnet
-@description : solve inverse kinematics problem with pseudo-inverse and damped least square
+@description : solve inverse kinematics problem with damped least square
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
- Global Constants
-'''
 NUM_SEGMENTS = 10
-INITIAL_ANGLE_RANGE = 0.0 #  [-angle, angle] in degrees for initial segment angle
-INITIAL_SEGMENT_LENGTH = 1000.0 # length each segment
 TARGET_POS = np.asarray([-500.0, 2000.0])
-
 REGULARIZATION_TERM = 0.1
 NUM_ITERATIONS = 100
 MAX_STEP_SIZE = 100
@@ -21,23 +15,22 @@ THRESHOLD = 1.0 # acceptable distance between the last vertex and target positio
 
 class Chain:
     def __init__(self):
-        self.angles = np.ones(NUM_SEGMENTS) * INITIAL_ANGLE_RANGE
-        self.lengths = np.ones(NUM_SEGMENTS) * INITIAL_SEGMENT_LENGTH
+        chain_length = 10000.0
+        segment_length = chain_length / NUM_SEGMENTS
+        self.angles = np.zeros(NUM_SEGMENTS)
+        self.lengths = np.ones(NUM_SEGMENTS) * segment_length
+        self.x = np.empty((NUM_SEGMENTS+1, 2)) # positions
+        computePositions(self)
 
-    def compute_positions(self):
-        num_pos = len(self.angles)+1
-        positions = np.empty((num_pos, 2))
-        positions[0] = [0, 0]
-        angle = 90.
-
-        for i in range(NUM_SEGMENTS):
-            angle += chain.angles[i]
-            positions[i+1][0] = np.cos(np.deg2rad(angle))
-            positions[i+1][1] = np.sin(np.deg2rad(angle))
-            positions[i+1] *= chain.lengths[i]
-            positions[i+1] += positions[i]
-
-        return positions
+def computePositions(chain):
+    chain.x[0] = [0, 0]
+    angle = 90.
+    for i in range(NUM_SEGMENTS):
+        angle += chain.angles[i]
+        chain.x[i+1][0] = np.cos(np.deg2rad(angle))
+        chain.x[i+1][1] = np.sin(np.deg2rad(angle))
+        chain.x[i+1] *= chain.lengths[i]
+        chain.x[i+1] += chain.x[i]
 
 '''
 Layout of the partial differential matrix of size(m x n)
@@ -49,10 +42,9 @@ x :  dx/da0     dx/da1     dx/da2
 y :  dy/da0     dy/da1     dy/da2
 '''
 def analyticJacobian(chain):
-    positions = chain.compute_positions()
     J = np.zeros(shape=(2,NUM_SEGMENTS))
     for i in range(NUM_SEGMENTS):
-        vec = positions[-1] -  positions[i]
+        vec = chain.x[-1] -  chain.x[i]
         # x = norm(vec) * cos(angle) and x' = norm(vec) * -sin(angle)
         # y = norm(vec) * sin(angle) and y' = norm(vec) * cos(angle)
         # hence x' = y * -1 and y' = x
@@ -71,7 +63,6 @@ def dampedLeastSquare(J):
     invJ = np.linalg.inv(invJ)
     return(J.transpose() * invJ)
 
-
 def printSingluarValues(matrix):
     '''
     Singular values analysis (for debugging)
@@ -87,11 +78,8 @@ def printSingluarValues(matrix):
 
 
 def inverseKinematic(chain):
-    '''
-    Solve inverse kinematic problem
-    '''
-    positions = chain.compute_positions()
-    vec = np.reshape(TARGET_POS - positions[-1], (2,1))
+    vec = np.reshape(TARGET_POS - chain.x[-1], (2,1))
+    #vec = TARGET_POS - x[-1]
     vec_norm = np.linalg.norm(vec)
     if (vec_norm > MAX_STEP_SIZE):
         vec /= vec_norm
@@ -108,19 +96,14 @@ def inverseKinematic(chain):
         chain.angles[i] += delta_angles[i]
 
 def hasReachTarget(chain):
-    '''
-    Return true if an 'acceptable' solution has been reached
-    '''
-    positions = chain.compute_positions()
-    if (np.linalg.norm(TARGET_POS - positions[-1]) <= THRESHOLD):
+    if (np.linalg.norm(TARGET_POS - chain.x[-1]) <= THRESHOLD):
         return True
     return False
 
 def show(chain):
-    pos = chain.compute_positions()
-    x, y = zip(*pos)
-    fig = plt.figure()
-    ax = plt.subplot()
+    x, y = zip(*chain.x)
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
     min_x, max_x = -5000., 5000.
     min_y, max_y = 0., 10500
     #ax.axis('equal') # FIXME - causes problem
@@ -143,5 +126,6 @@ if __name__ == '__main__':
         inverseKinematic(chain)
         print("IK : Iteration", iterations, "/", NUM_ITERATIONS )
         iterations += 1
+        computePositions(chain)
         show(chain)
 
--- miscellaneous/optimalTransformation.py
+++ miscellaneous/optimalTransformation.py
@@ -4,91 +4,71 @@
 Using eigen decomposition or svd decomposition
 """
 
-# TODO - add SVD decompostion
-# TODO - speed up covarience matrix with X^TX
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-'''
- Global Constants
-'''
 NUM_POINTS = 1000
 ROTATION = 39.0
 SCALE = [11.0, 4.0]
 TRANSLATE = [2.5, 1]
 
-def transformPoints(points, angle, scale, translate):
-    cosAngle = np.cos(np.deg2rad(angle))
-    sinAngle = np.sin(np.deg2rad(angle))
-
-    # create scale-rotation matrix
-    scaleRotationMat = np.matrix([[cosAngle * scale[0],  -sinAngle * scale[1]],
-                                  [sinAngle * scale[0],  cosAngle * scale[1]]])
-
-    transformedPoint = np.matmul(scaleRotationMat, points.T)
-    np.copyto(points, transformedPoint.T)
-    points += translate
+def transformPoints(points):
+    cosAngle = np.cos(np.deg2rad(ROTATION))
+    sinAngle = np.sin(np.deg2rad(ROTATION))
+    mat = np.asarray([[cosAngle * SCALE[0],  -sinAngle * SCALE[1]],
+                      [sinAngle * SCALE[0],  cosAngle * SCALE[1]]])
+    np.copyto(points, np.matmul(mat, points.T).T)
+    points += TRANSLATE
 
 def covariance(points, index0, index1):
     return np.sum(points[:,index0] * points[:,index1]) / len(points)
 
-def computeBestRotation(points, centroid):
-    # get direction
-    local_points = np.copy(points)
-    local_points -= centroid
+def computeBestRotation(local_points):
     # compute covariance matrix
     # cov(X,X) cov(X,Y)
     # cov(Y,X) cov(Y,Y)
     covXX = covariance(local_points,0,0)
     covYY = covariance(local_points,1,1)
     covXY = covariance(local_points,0,1)
-    covarianceMatrix = np.matrix([[covXX, covXY],
+    covariance_matrix = np.asarray([[covXX, covXY],
                                   [covXY, covYY]])
     # covariance decomposition
-    w, v = np.linalg.eig(covarianceMatrix)
-    rotationMatrix = v
-    det = np.linalg.det(rotationMatrix)
+    w, v = np.linalg.eig(covariance_matrix)
+    rotation_matrix = v
+    det = np.linalg.det(rotation_matrix)
     if det < 0.0:
         # from reflection matrix to rotation matrix
-        rotationMatrix *= -1.0
-    return rotationMatrix
+        rotation_matrix *= -1.0
+    return rotation_matrix
 
-def draw(points, centroid, rotationMatrix):
-    '''
-     Draw points + frames + bounding box
-    '''
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+def draw(points, centroid, rotation_matrix):
+    fig, ax = plt.subplots()
     ax.set_aspect('equal')
 
+    # compute min and max
+    inverse_matrix = rotation_matrix.T
     transformed_points = np.copy(points)
     transformed_points -= centroid
-    inverseRotationMatrix = rotationMatrix.T
-    transformed_points = np.asarray(np.matmul(inverseRotationMatrix, transformed_points.T).T)
-    boxMin = np.min(transformed_points, axis=0)
-    boxMax = np.max(transformed_points, axis=0)
+    transformed_points = np.matmul(inverse_matrix, transformed_points.T).T
+    box_min = np.min(transformed_points, axis=0)
+    box_max = np.max(transformed_points, axis=0)
 
     # draw the local frame
-    # in ax.arrow(...)  vec.y and vec.x are reversed !
-    vec0x = rotationMatrix.item(0,0)
-    vec0y = rotationMatrix.item(0,1)
-    vec1x = rotationMatrix.item(1,0)
-    vec1y = rotationMatrix.item(1,1)
-    ax.arrow(centroid[0], centroid[1], vec0y, vec0x, head_width=1.0, facecolor='red', edgecolor='black')
-    ax.arrow(centroid[0], centroid[1], vec1y, vec1x, head_width=1.0, facecolor='green', edgecolor='black')
+    eigvec0 = rotation_matrix[:,0]
+    eigvec1 = rotation_matrix[:,1]
+    ax.arrow(centroid[0], centroid[1], eigvec0[0], eigvec0[1], head_width=1.0, facecolor='red', edgecolor='black')
+    ax.arrow(centroid[0], centroid[1], eigvec1[0], eigvec1[1], head_width=1.0, facecolor='green', edgecolor='black')
 
     # draw points
     x, y = zip(*points)
     ax.scatter(x, y,s=0.5)
 
     # draw box
-    vertex = np.asarray([(boxMin[0], boxMin[1]), (boxMin[0], boxMax[1]),
-                         (boxMax[0], boxMax[1]), (boxMax[0], boxMin[1])])
+    vertex = np.asarray([(box_min[0], box_min[1]), (box_min[0], box_max[1]),
+                         (box_max[0], box_max[1]), (box_max[0], box_min[1])])
 
-    vertex = vertex = np.asarray(np.matmul(rotationMatrix, vertex.T).T)
+    vertex = np.matmul(rotation_matrix, vertex.T).T
     vertex += centroid
     polygon = patches.Polygon(vertex, closed=True,ec='darkgreen',lw=2,fill=False)
     ax.add_patch(polygon)
@@ -97,11 +77,14 @@ def draw(points, centroid, rotationMatrix):
     plt.show()
 
 if __name__ == '__main__':
-    points = np.array(np.random.rand(NUM_POINTS, 2) - 0.5)  # positions [-0.5, -0.5] [0.5, 0.5]
-    transformPoints(points, ROTATION, SCALE, TRANSLATE)
+    # create point cloud
+    points = np.random.rand(NUM_POINTS, 2) - 0.5
+    transformPoints(points)
 
-    # compute best transform (rotation, centroid)
+    # compute best transform (centroid + matrix)
     centroid = np.mean(points, axis=0)
-    rotationMatrix = computeBestRotation(points, centroid)
+    local_points = points - centroid
+    rotation_matrix = computeBestRotation(local_points)
 
-    draw(points, centroid, rotationMatrix)
+    # draw result
+    draw(points, centroid, rotation_matrix)
